bde68ae1f4de4874fc8f0c514bd8802d01093799
aaron <aaron@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2009-04-03 16:07:32 +0000

    Standardize the INSTALL Clean up the install dependencies and prepare environment scripts for LSReg and TS
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/RELEASE_3.1@2716 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,37 +2,31 @@
 
 MAKEROOT=""
 if [[ $EUID -ne 0 ]];
+then
     MAKEROOT="sudo "
 fi
 
-$MAKEROOT cpan base
 $MAKEROOT cpan Config::General
-$MAKEROOT cpan constant
 $MAKEROOT cpan Digest::MD5
 $MAKEROOT cpan English
 $MAKEROOT cpan Exporter
 $MAKEROOT cpan Fcntl
-$MAKEROOT cpan fields
 $MAKEROOT cpan File::Basename
 $MAKEROOT cpan Getopt::Long
 $MAKEROOT cpan IO::File
 $MAKEROOT cpan IO::Socket
 $MAKEROOT cpan IO::Socket::INET
 $MAKEROOT cpan IO::Socket::INET6
-$MAKEROOT cpan lib
-$MAKEROOT cpan Log::Log4perl
 $MAKEROOT cpan LWP::Simple
 $MAKEROOT cpan LWP::UserAgent
-$MAKEROOT cpan NetAddr::IP
+$MAKEROOT cpan Log::Log4perl
 $MAKEROOT cpan Net::DNS
 $MAKEROOT cpan Net::Ping
 $MAKEROOT cpan Net::Ping::External
-$MAKEROOT cpan Params::Validate
+$MAKEROOT cpan NetAddr::IP
 $MAKEROOT cpan POSIX
+$MAKEROOT cpan Params::Validate
 $MAKEROOT cpan Regexp::Common
-$MAKEROOT cpan Socket
-$MAKEROOT cpan Socket6
-$MAKEROOT cpan strict
 $MAKEROOT cpan Time::HiRes
-$MAKEROOT cpan warnings
 $MAKEROOT cpan XML::LibXML
+$MAKEROOT cpan base
